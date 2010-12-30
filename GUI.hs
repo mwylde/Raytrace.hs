8b304c04024ca95b5372855ceb6f8d4d25550516
Micah Wylde <mwylde@wesleyan.edu>
2010-12-29 22:28:02 -0800

    Added triangle surfaces

--- GUI.hs
+++ GUI.hs
@@ -2,16 +2,47 @@ import Geometry
 import Surfaces
 import Surfaces.Sphere
 import Surfaces.Plane
+import Surfaces.Triangle
 import Raytrace
 import Graphics.Rendering.OpenGL hiding (Color, Vector3, Light)
-import Graphics.UI.GLUT hiding (Color, Vector3, Light)
+import Graphics.UI.GLUT hiding (Color, Vector3, Light, Cube)
 
 red_color = Color 1 0 0
 green_color = Color 0 1 0
 white_color = Color 1 1 1 
 light_grey_color = Color 0.6 0.6 0.6
 black_color = Color 0 0 0
-purple_color = Color 0.6 0.2 0.5
+purple_color = Color 0.5 0.2 0.5
+greenish_color = Color 1 0.7 1
+
+-- Left, Right, Bottom, Top, Near, Far
+data Cube = Cube Float Float Float Float Float Float
+
+makeCube :: Material -> Cube -> [Surface]
+makeCube mat (Cube l r b t n f) = foldl doCube [] triangles where
+  doCube acc (a, b, c) = (makeTriangle a b c mat):acc
+  lbn = (Point3 l b n) -- left-bottom-near
+  lbf = (Point3 l b f) -- left-bottom-far
+  ltn = (Point3 l t n) -- etc.
+  ltf = (Point3 l t f)
+  rbn = (Point3 r b n)
+  rbf = (Point3 r b f)
+  rtn = (Point3 r t n)
+  rtf = (Point3 r t f)
+  triangles = [--(lbn, lbf, ltn), (lbf, ltn, ltf), -- left
+               --(rbn, rbf, rtn), (rbf, rtn, rtf), -- right
+               --(lbn, lbf, rbn), (lbf, rbn, rbf), -- bottom
+               --(ltn, ltf, rtn), (ltf, rtn, rtf), -- top
+               (lbn, ltn, rbn), (rtn, ltn, rbn)] -- near
+               --(lbf, ltf, rbf), (ltf, rbf, rtf)] -- far
+  
+cubes :: [Surface]
+cubes = (makeCube mat1 cube2) where
+--  cube1 = asd
+  cube2 = Cube 0 8 1 8 (-1) 8
+  mat1 = Material greenish_color white_color light_grey_color 100 (-1) 3
+    
+
 
 spheres :: [Surface] 
 spheres = foldl doSphere [] [0, (-3) .. (-40)] where
@@ -24,17 +55,20 @@ spheres = foldl doSphere [] [0, (-3) .. (-40)] where
     in s1:s2:s3:acc 
 
 plane :: Surface
-plane = makePlane (Point3 (-40) (-1) 2) (Point3 2 (-1) 2) (Point3 2 (-1) (-20)) plane_material where
+plane = makePlane (Point3 0 0 (-1)) (Point3 1 0 (-1)) (Point3 1 1 (-1)) plane_material where
   plane_material = Material light_grey_color white_color black_color 10 (-1) 4
   
 surfaces :: [Surface]
-surfaces = plane:spheres
+surfaces = plane:cubes where
+  mat1 = Material greenish_color white_color light_grey_color 100 (-1) 3
+  triangle = makeTriangle (Point3 0 0 1) (Point3 0 0 (-1))  (Point3 0 8 (-1)) mat1
 
 lights :: [Light]
-lights = [(Light (Point3 50 50 (-50)) (Color 0.75 0.75 0.75))]
+lights = [Light (Point3 50 2 100) (Color 1 1 1),
+          Light (Point3 4 12 20) (Color 0.2 0.2 0.2)]
 
 cameraFrame :: CameraFrame
-cameraFrame = cameraFromLookat (Point3 4 4 4) (Point3 (-1) (-1) (-1)) (Vector3 0 1 0)
+cameraFrame = cameraFromLookat (Point3 4 (-4) 4) (Point3 4 4 1) (Vector3 0 0 1)
 
 viewPlane :: ViewPlane
 viewPlane = ViewPlane 4 8 6
---
+++ Surfaces/Triangle.hs
@@ -0,0 +1,44 @@
+module Surfaces.Triangle ( makeTriangle
+                         ) where
+
+import Geometry
+import Surfaces
+
+makeTriangle :: Point3 -> Point3 -> Point3 -> Material -> Surface
+makeTriangle (Point3 ax ay az) (Point3 bx by bz) (Point3 cx cy cz) material =
+  Surface hit bbox material Nothing Nothing where
+    bbox = BBox 0 0 0 0 0 0
+    hit (Ray3 base dir) (t0, t1) =
+      if hit_t < t0 || hit_t > t1 then Nothing
+      else
+        let beta = (j*(e*i-h*f) + k*(g*f-d*i) + l*(d*h-e*g))/mm in
+        if beta < 0 || beta > 1 then Nothing
+        else
+          let gamma = (i*(a*k-j*b) + h*(j*c-a*l) + g*(b*l-k*c))/mm in
+          if gamma > 0 || (beta+gamma) > 1 then Nothing
+          else 
+            let pA = (Point3 ax ay az) 
+                a_to_b = vectorFrom pA (Point3 bx by bz)
+                a_to_c = vectorFrom pA (Point3 cx cy cz)
+                norm = normalize $ a_to_b `cross` a_to_c
+                point= (pvAdd pA (vmap (*beta) a_to_b))
+                point' = (pvAdd point (vmap (*gamma) a_to_c)) in
+            Just $ HitRecord material hit_t point' norm
+      where
+        -- Using the notation of Shirely & Marschner, Section 4.4.5
+        -- basically, using Cramer's rule to solve a system of linear equations
+        a = ax-bx
+        b = ay-by
+        c = az-bz
+        d = ax-cx
+        e = ay-cy
+        f = az-cz
+        g = (vX dir)
+        h = (vY dir)
+        i = (vZ dir)
+        j = ax-(pX base)
+        k = ay-(pY base)
+        l = az-(pZ base)
+        
+        mm = a*(e*i - h*f) + b*(g*f-d*i) + c*(d*h-e*g)
+        hit_t = (f*(a*k-j*b) + e*(j*c-a*l) + d*(b*l-k*c))/(-mm)
